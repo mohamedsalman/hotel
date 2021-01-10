e5ae5fab928e8fc03768df46d3cfe622df1fac5b
moodle <inspire.developersteam@gmail.com>
2021-01-10 14:15:54 +0200

    u

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -7,4 +7,54 @@ use Illuminate\Http\Request;
 class HotelController extends Controller
 {
     //
+    public function index()
+    {
+        $hotels = hotel::all();
+
+        return view('hotel.index', compact('hotels'));
+    }
+    public function create()
+    {
+        return view('hotel.create');
+    }
+    public function save(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'city' => 'required',
+            'country' => 'required'
+        ]);
+
+        Hotel::create($request->all());
+
+        return redirect()->route('hotel.index')
+            ->with('success', 'hotel created successfully.');
+    }
+    public function show()
+    {
+        return view('hotel.show', compact('hotel'));
+    }
+    public function edit()
+    {
+        return view('hotel.edit', compact('hotel'));
+    }
+    public function update(Request $request, hotel $hotel)
+    {
+        $request->validate([
+            'name' => 'required',
+            'city' => 'required',
+            'country' => 'required'
+        ]);
+        $hotel->update($request->all());
+
+        return redirect()->route('hotel.index')
+            ->with('success', 'Hotel updated successfully');
+    }
+    public function destroy(hotel $hotel)
+    {
+        $hotel->delete();
+
+        return redirect()->route('hotel.index')
+            ->with('success', 'Hotel deleted successfully');
+    }
 }
---
+++ resources/views/hotel/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Laravel 8 CRUD </h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('hotel.create') }}" title="Create a project"> <i class="fas fa-plus-circle"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered table-responsive-lg">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>City</th>
+            <th>Country</th>
+            <th>Date Created</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($hotels as $hotel)
+            <tr>
+                <td>{{ ++$i }}</td>
+                <td>{{ $hotel->name }}</td>
+                <td>{{ $hotel->city }}</td>
+                <td>{{ $hotel->country }}</td>
+                <td>{{ date_format($hotel->created_at, 'jS M Y') }}</td>
+                <td>
+                    <form action="{{ route('hotel.destroy', $hotel->id) }}" method="POST">
+
+                        <a href="{{ route('hotel.show', $hotel->id) }}" title="show">
+                            <i class="fas fa-eye text-success  fa-lg"></i>
+                        </a>
+
+                        <a href="{{ route('hotel.edit', $hotel->id) }}">
+                            <i class="fas fa-edit  fa-lg"></i>
+
+                        </a>
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" title="delete" style="border: none; background-color:transparent;">
+                            <i class="fas fa-trash fa-lg text-danger"></i>
+
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+@endsection
